92244a10293416a6d28478f79e5f286917705f87
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-04-18 15:01:09 +0100

    Added Login and Registration Page

--- react/src/components/Button.jsx
+++ react/src/components/Button.jsx
@@ -31,31 +31,31 @@ export const DarkButton = ({onclick, test}) => {
 
 // outline buttons
 export const PrimaryButtonOutline = ({onclick, test}) => {
+  return (
+    <button onclick={onclick} className='border-2 border-blue-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
+  )
+}
+
+export const SuccessButtonOutline = ({onclick, test}) => {
     return (
-      <button onclick={onclick} className='border-2 border-blue-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
+      <button onclick={onclick} className='border-2 border-green-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
     )
-  }
-  
-  export const SuccessButtonOutline = ({onclick, test}) => {
-      return (
-        <button onclick={onclick} className='border-2 border-green-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
-      )
-  }
-  
-  export const DangerButtonOutline = ({onclick, test}) => {
-      return (
-        <button onclick={onclick} className='border-2 border-red-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
-      )
-  }
-  
-  export const WarningButtonOutline = ({onclick, test}) => {
-      return (
-        <button onclick={onclick} className='border-2 border-yellow-500 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
-      )
-  }
-  
-  export const DarkButtonOutline = ({onclick, test}) => {
-      return (
-        <button onclick={onclick} className='border-2 border-red-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
-      )
-  }
+}
+
+export const DangerButtonOutline = ({onclick, test}) => {
+    return (
+      <button onclick={onclick} className='border-2 border-red-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
+    )
+}
+
+export const WarningButtonOutline = ({onclick, test}) => {
+    return (
+      <button onclick={onclick} className='border-2 border-yellow-500 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
+    )
+}
+
+export const DarkButtonOutline = ({onclick, test}) => {
+    return (
+      <button onclick={onclick} className='border-2 border-red-600 text-black font-bold py-2 px-2 rounded-md' >{test}</button>
+    )
+}
